d960ba3318ed8bb8a88fbafd94a022e67f1fdb7d
Alan-italiano <92887382+Alan-italiano@users.noreply.github.com>
2024-08-26 16:54:05 -0300

    Update alb-certificate.tf

--- alb-certificate.tf
+++ alb-certificate.tf
@@ -2,19 +2,19 @@ module "acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "~> 4.0"
 
-  domain_name  = "var.domain_name"
-  zone_id      = "var.route_53_zone_id"
+  domain_name  = var.domain_name
+  zone_id      = var.route_53_zone_id
 
   validation_method = "DNS"
 
   subject_alternative_names = [
-    "var.cert_san_1",
-    "var.cert_san_2"
+    var.cert_san_1,
+    var.cert_san_2
   ]
 
   wait_for_validation = true
 
   tags = {
-    Name = "var.domain_name"
+    Name = var.domain_name
   }
 }
