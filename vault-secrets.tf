130ac84dccfbc7622cb39d7257181e299c2e6b33
Alan-italiano <92887382+Alan-italiano@users.noreply.github.com>
2024-08-26 16:30:51 -0300

    Update vault-secrets.tf

--- vault-secrets.tf
+++ vault-secrets.tf
@@ -1,18 +1,5 @@
 ######### CREATE VAULT SECRETS (CERTS, LICENSE AND KMS) #################
 
-resource "kubernetes_secret" "vault-secret" {
-  metadata {
-    name      = "vault-secret"
-    namespace = "vault"
-  }
-  data = {
-    license = ""
-  }
-  type = "Opaque"
-  depends_on = [kubernetes_namespace.vault]
-}
-
-
 data "template_file" "vault-aws-key" {
   template = "${file("vault-aws.key")}"
 }
@@ -23,6 +10,23 @@ data "template_file" "vault-aws-crt" {
 
 data "template_file" "vault-aws-ca" {
   template = "${file("vault-aws.ca")}"
+}
+
+data "template_file" "vault-secret" {
+  template = "${file("vault-lic.hclic")}"
+}
+
+
+resource "kubernetes_secret" "vault-secret" {
+  metadata {
+    name      = "vault-secret"
+    namespace = "vault"
+  }
+  data = {
+    license = data.template_file.vault-secret.template
+  }
+  type = "Opaque"
+  depends_on = [kubernetes_namespace.vault]
 }
 
 resource "kubernetes_secret" "vault-acm-cert" {
